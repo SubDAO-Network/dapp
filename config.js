5df0693c915d0a013ba75ba4e09b43db36c841dd
July789 <>
2023-03-08 09:08:37 +0800

    format code

--- config.js
+++ config.js
@@ -4,40 +4,41 @@ window.mainAddress = {
   actionVaultOptToken: "0xFC3E98bEe5de56118f1eEcA48DAD91889B11c239",
   // for deposit or withdraw token
   actionVaultDeposit: "0x2021D9265B130B6B23F0f70Cd2CaFf98c710eeB1",
-  
+
   // for vote
-  actionVoteNew: '0x8e2c198575ED200EC9B958F4784B8193757580bc',
+  actionVoteNew: "0x8e2c198575ED200EC9B958F4784B8193757580bc",
   actionVoteVote: "0x77E54EFeEf02146C6e90661b676Be1D269a8a86F",
-  actionVoteExecute: '0xd95a33ecB11C78b1FF4D36073291b08B900Fe14A',
-  actionVaultWithdrawToken: '0x15c77f1EE3Fd6391ee8FCfdFf8C4930e57a149Ab',
-  actionVaultExeWithdrawToken: '0x3A4cDe6D856cE7421b563FE50f54e6731196E18a',
-  actionGrantMethodRegister: '0x0bBC94675908de91D805D71EAf589BD71a26863B',
-  actionGrantMethodApply: '0x3D8518293cdF8143dD464A5Fb538e48faF68993E',
-  actionVoteImpeachRegister: '0x23D2E0CdFE6A2D110bccf5cFCfed5FE92569a900',
-  actionVoteImpeachApply: '0xF35374010110B7293F00a314071955Bf3F951e1d',
-  actionVoteTransferPeriodRegister: '0x493D739ba5d5bDAc5638f719CD7CF6244D6dD92c',
-  actionVoteTransferPeriodApply: '0xEd3D5709C7e572443Ec183645B447A3349f7bC04',
+  actionVoteExecute: "0xd95a33ecB11C78b1FF4D36073291b08B900Fe14A",
+  actionVaultWithdrawToken: "0x15c77f1EE3Fd6391ee8FCfdFf8C4930e57a149Ab",
+  actionVaultExeWithdrawToken: "0x3A4cDe6D856cE7421b563FE50f54e6731196E18a",
+  actionGrantMethodRegister: "0x0bBC94675908de91D805D71EAf589BD71a26863B",
+  actionGrantMethodApply: "0x3D8518293cdF8143dD464A5Fb538e48faF68993E",
+  actionVoteImpeachRegister: "0x23D2E0CdFE6A2D110bccf5cFCfed5FE92569a900",
+  actionVoteImpeachApply: "0xF35374010110B7293F00a314071955Bf3F951e1d",
+  actionVoteTransferPeriodRegister:
+    "0x493D739ba5d5bDAc5638f719CD7CF6244D6dD92c",
+  actionVoteTransferPeriodApply: "0xEd3D5709C7e572443Ec183645B447A3349f7bC04",
 
   // for org
   actionOrgMgr: "0x23bE5b7e4AfF9e6c281df499A3472ecDc48d829e",
   actionOrgMemberQuit: "0xa4152c13003b3f7f28D83c0831d4E8052FcA249D",
   actionOrgTransferOwner: "0x15D717f1Efc665CbFe3B901a8689A7DC63acd77E",
-  actionOrgSetJoinMode: '0x038DCcCCd6FFa63F6a358A1a99Ba0a4bF6427F93',
-  actionOrgApplyJoin: '0x1Db1301549b43837F10F0c530BD63CA9a95aD59D',
+  actionOrgSetJoinMode: "0x038DCcCCd6FFa63F6a358A1a99Ba0a4bF6427F93",
+  actionOrgApplyJoin: "0x1Db1301549b43837F10F0c530BD63CA9a95aD59D",
 
   // for venture
-  actionVenturesRaiseMoney: '0x2368d54b4dCc553ff2377641bBFDc1Fe762A5E09',
-  actionVenturesRaiseFail: '0x977F77541DCdeAf7b8D606Bd40b4299240Dea2a9',
-  actionVenturesOpenGPRaise: '0x95f608cc3E575F8677d4d3AC5E35765faA166B5f',
-  actionVenturesOpenLPRaise: '0x695C01E2eAeE4c418b5990a2bC9A33C731C07A02',
-  actionVenturesToRaiseFail: '0x131CE4Da3f9DcC249f571897f7B06608d746FDCB',
-  actionVenturesRaiseFailClaim: '0x97689aE0803afd488E1e6E7B6ac6648284607Af5',
-  actionVenturesToFreezingPeriod: '0xa375E87f4CCc0eaf086f907effB42CBf6D56A961',
-  actionVenturesClaimShare: '0xFB7E7701cb50137D5972EeC24f8585ec4E466c8c',
-  actionVaultUniswapV2SwapToken: '0xD51b6b3ae4ec43A141184e37c70d07B31E5f12Ce',
+  actionVenturesRaiseMoney: "0x2368d54b4dCc553ff2377641bBFDc1Fe762A5E09",
+  actionVenturesRaiseFail: "0x977F77541DCdeAf7b8D606Bd40b4299240Dea2a9",
+  actionVenturesOpenGPRaise: "0x95f608cc3E575F8677d4d3AC5E35765faA166B5f",
+  actionVenturesOpenLPRaise: "0x695C01E2eAeE4c418b5990a2bC9A33C731C07A02",
+  actionVenturesToRaiseFail: "0x131CE4Da3f9DcC249f571897f7B06608d746FDCB",
+  actionVenturesRaiseFailClaim: "0x97689aE0803afd488E1e6E7B6ac6648284607Af5",
+  actionVenturesToFreezingPeriod: "0xa375E87f4CCc0eaf086f907effB42CBf6D56A961",
+  actionVenturesClaimShare: "0xFB7E7701cb50137D5972EeC24f8585ec4E466c8c",
+  actionVaultUniswapV2SwapToken: "0xD51b6b3ae4ec43A141184e37c70d07B31E5f12Ce",
 
-  tokenAddress: '0xbaDd22fA317b453f9343eA8A3971a0E0102899a2',
-  tokenSymbol:'GOV',
-  tokenDecimals:18,
-  tokenImage:'https://avatars.githubusercontent.com/u/74033957?s=200&v=4'
+  tokenAddress: "0xbaDd22fA317b453f9343eA8A3971a0E0102899a2",
+  tokenSymbol: "GOV",
+  tokenDecimals: 18,
+  tokenImage: "https://avatars.githubusercontent.com/u/74033957?s=200&v=4",
 };
